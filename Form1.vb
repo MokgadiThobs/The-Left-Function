9ceefd6a86ea2765f2abceb6d43e5f655ee76238
Dell-User <Dell-User@DESKTOP-8EU8MHV>
2023-10-05 04:11:47 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class BtnShow
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim MyText As String
+        MyText = "Visual Basic"
+        MsgBox(Microsoft.VisualBasic.Left(MyText, 4))
+    End Sub
+End Class
